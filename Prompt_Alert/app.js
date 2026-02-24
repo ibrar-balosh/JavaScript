221dab4b0bd781bf30859359a93ef217a5ab9b4a
ibrar-balosh <ibrarkurd3@gmail.com>
2026-02-24 14:45:58 +0500

    prompt and string done

---
+++ Prompt_Alert/app.js
@@ -0,0 +1,36 @@
+
+// Welcome  Message
+
+// var username = prompt ("Enter your name!");
+// alert("well come"+" "+ username);
+// console.log("wellcome" + "  " + username)
+
+// Age Message
+
+// var username = prompt ("Enter your identity please!");
+// var age = prompt ("Enter your age!");
+// alert(username + " " + "is" + " " + age + " " + "years old");
+// console.log(username + " " + "is" + " " + age + " " + "years old");
+
+// Favorite Color Message
+
+// var username = prompt ("Enter your name!");
+// var favtcolor = prompt ("Enter your favt color!");
+// alert(username + "'s  " +  "favt color is" + "  " + favtcolor);
+// console.log(username + "'s  " +  "favt color is" + "  " + favtcolor);
+
+// Simple Math Sentence
+
+// var num1 = +prompt("Enter number from your side! ");
+// var num2 = +prompt("Enter number from your side! ");
+// var sum = num1 + num2;
+// console.log("the result of " + num1 + " + " + num2 + " " + "is" + " " + sum);
+
+// About Me Summary
+
+// var username = prompt ("Enter your identity!");
+// var country = prompt ("Enter your country!");
+// var hobby = prompt ("Enter your hobby!");
+// alert("Hi! I am" + " " + username + " " + "from" + " " + country + " " + "and i love" + " " + hobby + ".");
+// console.log("HI I am" + " " + username + " " + "from" + " " + country + " " + "and i love" + " " + hobby + ".");
+
---
+++ String/app.js
@@ -0,0 +1,32 @@
+// Strings
+
+// Create a full address
+
+// var street = prompt ("Enter your street name!");
+// var city = prompt ("Enter your city name!");
+// var country = prompt ("Enter your country name!");
+// console.log("Your full address is:" + " " + street + " " + city + " " + country + ".");
+
+
+// Make a custom username
+
+// var username = prompt ("Enter your name!");
+// var favouritenumber = prompt ("Enter your favourite number!");
+// console.log("Your new user is:" + " " + username + favouritenumber + ".");
+
+// Email Generator
+
+// var firstname = prompt ("enter your first name");
+// var lastname = prompt ("enter your last name");
+// console.log(firstname + lastname + "@gmail.com");
+
+// Combine Favorite Things
+
+// var favtcolorcolor = prompt ("enter your color name");
+// var favouriteanimal = prompt ("enter your animal name");
+// var favouritefood = prompt ("enter your food name");
+// console.log("YOUR FAVT THINGS ARE:" + " " + favtcolorcolor + " " + favouriteanimal + " " + "And" + " " + favouritefood + ".");
+
+var favtquote = prompt ("Enter your favt quote:");
+var authorname = prompt ("Enter your author's name:");
+console.log('"' + favtquote + '."  - ' + authorname);
