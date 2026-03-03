8731326dab78b18c39b81ed14af84bf7a2d14e7f
ibrar-balosh <ibrarkurd3@gmail.com>
2026-03-03 16:11:16 +0500

    all done

---
+++ If-else/app.js
@@ -0,0 +1,243 @@
+// if else Statement
+
+// 1
+// var age = prompt("Enter Your Age:");
+
+// if (age >= 18){
+//     console.log("You are an adult.");    
+// }
+// else{
+//     console.log("You are a minor.");
+// }
+
+// 2
+// var positivenum = prompt("Enter Number:");
+
+// if (positivenum >= 0){
+//     console.log("it's Positive Number.");    
+// }
+// else{
+//     console.log("Not Positive.");
+// }
+
+// 3
+// var number = prompt("Enter a number:");
+
+// if (number % 2 == 0) {
+//     console.log("Even number");
+// } else {
+//     console.log("Odd number");
+    
+// }
+
+
+// 4
+// var username = prompt("Enter your identity:")
+
+// if (username === "Admin"){
+//     console.log("Welcome, admin!");
+// }
+// else {
+//     console.log("Access denied.");
+// }
+
+// 5
+// var num1 = Number(prompt("Enter first number:"));
+// var num2 = Number(prompt("Enter second number:"));
+
+// if (num1 === num2) {
+//   console.log("Both numbers are the same");
+// } else {
+//   console.log("The numbers are different");
+// }
+
+// 6
+// var num1 = Number(prompt("Enter first number:"));
+// var num2 = Number(prompt("Enter second number:"));
+
+// if (num1 > num2) {
+//   alert("First is larger");
+// } else if (num2 > num1) {
+//   alert("Second is larger");
+// } else {
+//   alert("Both numbers are equal");
+// }
+
+// 7
+// var num = prompt("Enter number:");
+
+// if (num > 100) {
+//   alert("Too big!");
+// } else {
+//   alert("Within range.");
+// }
+
+// 8   
+// var num = prompt("Enter Passing number:");
+
+// if (num >= 50 && num <= 100) {
+//   alert("You Passed!");
+// } 
+// else if (num > 100) {
+//     alert("Enter the correct number ");
+// }
+// else { 
+//   alert("You failed.");
+// }
+
+// 9
+// var num = prompt("Enter a temperature:");
+
+// if (num > 30) {
+//   alert("Too hot!");
+// } else {
+//   alert("it's not hot.");
+// }
+
+// 10
+// var num = prompt("Enter a number:");
+
+// if (num == 0) {
+//   alert("it's zero!");
+// } else {
+//   alert("Not zero.");
+// }
+
+// 11
+// var age = prompt("Enter Passing number:");
+
+// if (age >= 60) {
+//   console.log("You are a senior citizen.",);
+// } 
+// else { 
+//   console.log("You are not a senior.");
+// }
+
+// 12
+// var name = prompt("What is your name?");
+
+// if (name === "Alice") {
+//   alert("Hi, Alice!");
+// } else {
+//   alert("You're not Alice!");
+// }
+
+// // 13
+// var code = prompt("What is password?");
+
+// if (code === "08790") {
+//   console.log("Correct password");
+// } else {
+//   console.log("Wrong password");
+// }
+
+// // 14
+// var hour = prompt("Type current hour:");
+
+// if (hour <= 12) {
+//   alert("Good morning!");
+// } else if (hour <= 24) {
+//   alert("Good afternoon!");
+// }
+// else {
+//     alert("Type correct hour!");
+// }
+
+// 15
+// var number = prompt("Enter a number:");
+
+// if (number % 5 == 0) {
+//     console.log("Divisible by 5");
+// } else {
+//     console.log("Not divisible by 5");
+    
+// }
+
+// 16
+// var play = prompt("Are you want to play a game?:");
+
+// if (play === "yes" || play === "Yes") {
+//   alert("Let's play!");
+// } 
+// else if (play === "no" || play === "No") {
+//   alert("Maybe next time!");
+// }
+// else {
+//     alert("answer with correct text!");
+// }
+
+// 17
+// var color = prompt("Type a color name:").toLowerCase();
+
+// if (color === "blue") {
+//   console.log(color + " is " + "Cool choice!");
+// } 
+// else if (
+//   color === "black" ||
+//   color === "white" ||
+//   color === "red" ||
+//   color === "green" ||
+//   color === "yellow" ||
+//   color === "blue" ||
+//   color === "brown" ||
+//   color === "orange" ||
+//   color === "pink" ||
+//   color === "purple" ||
+//   color === "gray"
+// ) {
+//   console.log(color + " is " + "Interesting color");
+// }
+// else {
+//   console.log("Answer with valid colors!");
+// }
+
+// 18
+// var day = prompt("Type day of the week:").toLowerCase();
+
+// if (day === "sunday") {
+//   console.log("Weekend!");
+// } 
+// else if (
+//   day === "monday" ||
+//   day === "tuesday" ||
+//   day === "wednesday" ||
+//   day === "thursday" ||
+//   day === "friday" ||
+//   day === "saturday"
+// ) {
+//   console.log("Weekday!");
+// }
+// else {
+//   console.log("Answer with valid day!");
+// }
+
+// 19
+// var fruit = prompt("Type a fruit name:").toLowerCase();
+
+// if (fruit === "banana" || fruit === "apple") {
+//   alert("Special stock available!");
+// } 
+// else if (
+//   fruit === "mango" ||
+//   fruit === "orange" ||
+//   fruit === "peach" ||
+//   fruit === "grapes" ||
+//   fruit === "watermelon" ||
+//   fruit === "strawberry" ||
+//   fruit === "cherry"
+// ) {
+//   alert("We have that!");
+// }
+// else {
+//   alert("Sorry, out of stock!");
+// }
+
+// 20
+// var start = prompt("Start!").toLowerCase();
+
+// if (start === "start") {
+//     alert("Game started!");
+// }
+// else {
+// alert("Waiting for input.....");
+// }
